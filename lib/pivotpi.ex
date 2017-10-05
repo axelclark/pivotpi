0e8a1451e94630c02a8a2e4fed22baa8d1a74ba1
Axel Clark <axelclark2@yahoo.com>
2017-10-04 22:00:00 -0400

    Initial commit

---
+++ lib/pivotpi.ex
@@ -0,0 +1,18 @@
+defmodule PivotPi do
+  @moduledoc """
+  Documentation for PivotPi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PivotPi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pivotpi_test.exs
@@ -0,0 +1,8 @@
+defmodule PivotPiTest do
+  use ExUnit.Case
+  doctest PivotPi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
